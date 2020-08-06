855c399a4fd741441d822f34a4cbddfe4237b798
E. Joshua Rigler <erigler@usgs.gov>
2020-08-06 10:48:42 -0600

    Switch map generation to Cartopy from Basemap
    Basemap has not been supported for several years. Cartopy replaces
    most Basemap functionality, but does so using a very different API.
    There might also be a few changes here related to Py2->Py3 transition.

--- bin/make_imp_gp.py
+++ bin/make_imp_gp.py
@@ -21,7 +21,7 @@ This script does NOT:
   is handled by make_impmaps.py)
 
 """
-from __future__ import print_function
+
 import numpy as np
 from glob import glob
 
@@ -78,10 +78,10 @@ if __name__ == "__main__":
    # if custom interval is required, modify the following lines to override the
    # realtime interval calcualted from min_obs_age and every_nth, otherwise set
    # starttime and endtime equal to None
-   # starttime = UTCDateTime(2018,8,1,0,0,0)
-   # endtime = UTCDateTime(2018,8,1,1,0,0)
-   starttime = None
-   endtime = None
+   starttime = UTCDateTime(2020,4,20,9,0,0)
+   endtime = UTCDateTime(2020,4,20,12,0,0)
+   # starttime = None
+   # endtime = None
 
    # output formatting
    write_CDF = False
@@ -102,9 +102,9 @@ if __name__ == "__main__":
    # construct prediction grid
    lat_tmp, lon_tmp, r_tmp = np.meshgrid(
       np.linspace(pred_lat[0], pred_lat[1],
-                  (pred_lat[1] - pred_lat[0]) / pred_lat[2] + 1),
+                  (pred_lat[1] - pred_lat[0]) // pred_lat[2] + 1),
       np.linspace(pred_lon[0], pred_lon[1],
-                  (pred_lon[1] - pred_lon[0]) / pred_lon[2] + 1),
+                  (pred_lon[1] - pred_lon[0]) // pred_lon[2] + 1),
       Re,
       indexing='ij'
    )
